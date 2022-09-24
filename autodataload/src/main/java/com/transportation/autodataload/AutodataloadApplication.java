482938904b12e19b9c198497e293d1c96f01dabb
Gabriel Albuquerque <gabriel.albuquerque.dev@gmail.com>
2022-09-24 19:26:05 +0100

    add initial spring project

---
+++ autodataload/src/main/java/com/transportation/autodataload/AutodataloadApplication.java
@@ -0,0 +1,13 @@
+package com.transportation.autodataload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutodataloadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutodataloadApplication.class, args);
+	}
+
+}
---
+++ autodataload/src/test/java/com/transportation/autodataload/AutodataloadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transportation.autodataload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutodataloadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
