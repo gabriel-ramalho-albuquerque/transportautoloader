999c680778e57fc0c908a6d2f30df8e706f2118e
Gabriel Albuquerque <gabriel.albuquerque.dev@gmail.com>
2022-09-26 06:55:35 +0100

    add yellow trip reader config class

--- autodataload/src/main/java/com/transportation/autodataload/config/batch/reader/YellowTripReaderConfig.java
+++ autodataload/src/main/java/com/transportation/autodataload/config/batch/reader/YellowTripReaderConfig.java
@@ -1,6 +1,6 @@
 package com.transportation.autodataload.config.batch.reader;
 
-import com.transportation.autodataload.model.Trip;
+import com.transportation.autodataload.model.YellowTripDTO;
 import lombok.AllArgsConstructor;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.LineMapper;
@@ -10,16 +10,16 @@ import org.springframework.core.io.FileSystemResource;
 
 @Configuration
 @AllArgsConstructor
-public class TripReaderConfig implements ReaderConfig<Trip> {
+public class YellowTripReaderConfig implements ReaderConfig<YellowTripDTO> {
 
-    private final LineMapper<Trip> tripLineMapper;
+    private final LineMapper<YellowTripDTO> tripLineMapper;
 
-    @Bean("TripReader")
+    @Bean("YellowTripReader")
     @Override
-    public FlatFileItemReader<Trip> reader() {
-        FlatFileItemReader<Trip> itemReader = new FlatFileItemReader<>();
+    public FlatFileItemReader<YellowTripDTO> reader() {
+        FlatFileItemReader<YellowTripDTO> itemReader = new FlatFileItemReader<>();
         itemReader.setResource(new FileSystemResource("src/main/resources/csv/yellowTrip/yellow_tripdata_2018-01_01-15.csv"));
-        itemReader.setName("Trip reader");
+        itemReader.setName("Yellow Trip reader");
         itemReader.setLineMapper(tripLineMapper);
 
         return itemReader;
